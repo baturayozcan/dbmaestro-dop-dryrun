122ccf8d6da5ab5d374848676cc01d1b3c69b168
baturayozcan <baturayozcan>
2023-06-06 10:52:45 +0000

    create sp

---
+++ PROCEDUREs/Creation-Scripts/GETEMPLOYEEFIRSTNAME.sql
@@ -0,0 +1,10 @@
+CREATE OR REPLACE EDITIONABLE PROCEDURE GETEMPLOYEEFIRSTNAME (emp_first_name OUT VARCHAR2)
+IS
+BEGIN
+  SELECT first_name
+  INTO emp_first_name
+  FROM employees
+  WHERE first_name='şşşüüüiiiööö';
+END;
+/
+
